4c2efc806a5bf494a632fe972721b3d31be1d8d3
Cal Freeman <CalFreeman@email.com>
2019-10-03 13:12:01 -0500

    working word parser from user provided input file

--- main-cmd.py
+++ main-cmd.py
@@ -1,2 +1,20 @@
 # Caleb Freeman
 # Brandon Sharp
+
+print("Please enter input file name")
+
+with open(input(), 'rU') as f:
+    dataArray = []
+    lines = f.read().split()
+    try:
+        for line in lines:
+            dataArray.append(line)
+    except ValueError:
+        # writing error output to output file
+        with open('WordsCounts.txt', 'w') as the_file:
+            the_file.write('Exception: Input file must only contain...\n')
+        exit(1)
+
+    len = len(dataArray)
+for x in range(len):
+    print(dataArray[x])
