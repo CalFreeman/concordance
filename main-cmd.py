f6e29f814b85dae8e32cf293d0681c1b03a5facf
Cal Freeman <CalFreeman@email.com>
2019-10-03 13:27:21 -0500

    reading character by character

--- main-cmd.py
+++ main-cmd.py
@@ -8,13 +8,26 @@ with open(input(), 'rU') as f:
     lines = f.read().split()
     try:
         for line in lines:
-            dataArray.append(line)
+            for ch in line:
+                dataArray.append(ch)
     except ValueError:
         # writing error output to output file
         with open('WordsCounts.txt', 'w') as the_file:
             the_file.write('Exception: Input file must only contain...\n')
         exit(1)
 
-    len = len(dataArray)
-for x in range(len):
+lenW = len(dataArray)
+
+for x in range(lenW):
     print(dataArray[x])
+#    characters = []
+#    for letter in (dataArray[x]):
+#        characters.append(letter)
+
+#lenC = len(characters)
+
+#for x in range(lenC):
+#    print(characters[x])
+
+print("")
+print("done...")
